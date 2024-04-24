4bd333e4512a5eb71359db8060d0db13b959feee
Jaime <jaime.tellie@gmail.com>
2024-04-24 17:08:38 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'levain_d',
+  webDir: 'www'
+};
+
+export default config;
