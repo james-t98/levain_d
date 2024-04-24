06fc609b7b97668fe1b13b1e2d1ce0060d7949db
Jaime <jaime.tellie@gmail.com>
2024-04-24 17:20:14 +0200

    Created the initial pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,31 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  },
+  // {
+  //   path: 'contact',
+  //   loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
+  // },
+  // {
+  //   path: 'splash',
+  //   loadChildren: () => import('./pages/splash/splash.module').then( m => m.SplashPageModule)
+  // },
+  // {
+  //   path: 'signup',
+  //   loadChildren: () => import('./pages/signup/signup.module').then( m => m.SignupPageModule)
+  // },
+  // {
+  //   path: 'login',
+  //   loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  // },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  // },
+  // {
+  //   path: 'profile',
+  //   loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  // }
 ];
 @NgModule({
   imports: [
